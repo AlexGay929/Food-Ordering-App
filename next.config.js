615b2b3d683808872991f350d184ad1af94eeb41
Alex Gay <alexgay27753@gmail.com>
2023-08-22 05:51:43 +0800

    config commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true, // Enable React strict mode
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx'], // Specify file extensions for pages
+    images: {
+    domains: ['example.com'], // Configure domains for Image component
+  },
+}
 
 module.exports = nextConfig
