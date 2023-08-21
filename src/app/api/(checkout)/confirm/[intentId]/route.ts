57906473621be8bc296ad58f5b0dc4618ef8b056
Alex Gay <alexgay27753@gmail.com>
2023-08-22 05:21:19 +0800

    intentId commit

--- src/app/api/(checkout)/confirm/[intentId]/route.ts
+++ src/app/api/(checkout)/confirm/[intentId]/route.ts
@@ -1,7 +1,12 @@
 import { prisma } from "@/utils/connect";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const PUT = async ({ params }: { params: { intentId: string } }) => {
+
+// export const PUT = async ({ params }: { params: { intentId: string } }) => {
+  export async function PUT(
+    request: NextRequest,
+    { params }: { params: { intentId: string } }
+  ) {
   const { intentId } = params;
 
   try {
