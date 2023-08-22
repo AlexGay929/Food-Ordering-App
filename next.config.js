e3ef1fd360dd3529ffda5be6ac9edbd072d8c5ef
Alex Gay <alexgay27753@gmail.com>
2023-08-23 07:01:27 +0800

    localhost commit

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
         ignoreDuringBuilds: true
       },
       env: {
-        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:3000',
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
       },
 }
 
