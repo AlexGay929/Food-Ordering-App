3cd09d2011396b7b585a6e872ed51b13b2369461
Alex Gay <alexgay27753@gmail.com>
2023-08-24 07:44:25 +0800

    json commit

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -6,7 +6,7 @@ import DeleteButton from '@/components/DeleteButton'
 import axios from 'axios'
 
 const getData = async (id:string)=>{
-  const res = await fetch(`https://raw.githubusercontent.com/AlexGay929/Food-Ordering-App/src/app/api/products/${id}`,{
+  const res = await fetch(`https://raw.githubusercontent.com/AlexGay929/Food-Ordering-App/src/app/api/products/${id}.json`,{
     cache:"no-store"
   })
 
